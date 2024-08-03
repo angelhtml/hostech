a0567573bb3e272016a626bdaa6575d67adddbbc
Angel <82230757+angelhtml@users.noreply.github.com>
2024-08-04 03:04:10 +0330

    Add files via upload

--- src/components/herrosecrion.tsx
+++ src/components/herrosecrion.tsx
@@ -4,9 +4,9 @@ export default function Herrosection(){
     return(
         <div className="z-0 relative top-0
         xl:top-12">
-            <img className="h-screen w-screen bg-center" src="/public/images/downl434fdoad.jpg" alt="herro"/>
-            <img className="absolute top-0 right-0" src="/public/images/dowhhghgz321nload.png" alt="points"/>
-            <img className="absolute top-60 left-5" src="/public/images/down6565tload.png" alt="points"/>
+            <img className="h-screen w-screen bg-center" src="/images/downl434fdoad.jpg" alt="herro"/>
+            <img className="absolute top-0 right-0" src="/images/dowhhghgz321nload.png" alt="points"/>
+            <img className="absolute top-60 left-0" src="/images/down6565tload.png" alt="points"/>
         </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,14 @@
 import {MdOutlineEmail, MdOutlinePhone, MdOutlineChat, MdPersonOutline, MdOutlineSearch, MdOutlineMenu} from "react-icons/md";
 import { FaAngleDown } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 export default function Navbar(){
+
+    const [hovername, setHovername] = useState<string>()
+
+    useEffect(() => {
+        console.log(hovername)
+    },[hovername])
     return(
 
         <div className="z-50 fixed w-full">
@@ -18,16 +25,26 @@ export default function Navbar(){
 
             <div className="w-full h-12 flex">
                 <div className="flex-1 flex">
-                    <button className="mx-10"><img src="/public/images/white-logo.svg" alt="icon"/></button>
+                    <button className="mx-10"><img src="/images/white-logo.svg" alt="icon"/></button>
                 </div>
                 <ul className="hidden text-white font-medium
                 xl:flex xl:flex-4 xl:justify-around xl:items-center">
-                    <li className="flex cursor-pointer">Home<FaAngleDown className="mx-1 text-lg relative top-1"/></li>
-                    <li className="flex cursor-pointer">Pages<FaAngleDown className="mx-1 text-lg relative top-1"/></li>
+
+                    <li onMouseEnter={()=> setHovername("home")} onMouseLeave={()=> setHovername("")}>
+                        <span className="flex cursor-pointer hover:text-orange-500">Home<FaAngleDown className="mx-1 text-lg relative top-1"/></span>
+                        {hovername == "home" && <ul className="bg-white absolute top-22 w-72 h-48">...</ul>}
+                    </li>
+
+                    <li onMouseEnter={()=> setHovername("pages")} onMouseLeave={()=> setHovername("")}>
+                        <span className="flex cursor-pointer">Pages<FaAngleDown className="mx-1 text-lg relative top-1"/></span>
+                        {hovername == "pages" && <ul className="bg-white absolute top-22 w-72 h-48">pages</ul>}
+                    </li>
+
                     <li className="flex cursor-pointer">Hosting<FaAngleDown className="mx-1 text-lg relative top-1"/></li>
                     <li className="flex cursor-pointer">Doming</li>
                     <li className="flex cursor-pointer">News<FaAngleDown className="mx-1 text-lg relative top-1"/></li>
                     <li className="flex cursor-pointer">Help Center<FaAngleDown className="mx-1 text-lg relative top-1"/></li>
+
                 </ul>
                 <div className="flex-1 flex justify-end">
                     <button className=" text-white text-2xl mx-2"><MdOutlineSearch /></button>
